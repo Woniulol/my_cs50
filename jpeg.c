eea071d7042e470e855705b653a1d73b64101b57
Jianan Wang <jnwangcn@gmail.com>
2024-09-22 13:27:04 +0800

    update: jpeg

---
+++ jpeg.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        return 1;
+    }
+
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL)
+    {
+        return 1;
+    }
+
+    char bytes[3];
+    // Read from file, read 3 times every time 1 byte from file and assign to bytes
+    fread(bytes, 3, 1, file);
+
+    // Check is bytes are following the JPEG format
+    if (bytes[0] == 0xff && bytes[0] == 0xd8 && bytes[0] == 0xff)
+    {
+        printf("Maybe\n");
+    }
+    else
+    {
+        printf("No\n");
+    }
+}
+
